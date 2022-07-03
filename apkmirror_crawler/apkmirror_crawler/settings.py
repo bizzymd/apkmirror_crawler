f76d27e539948088469e56c1653ccad0e3049744
Savin Cristian <savin.cristian99@gmail.com>
2022-07-03 14:44:01 +0200

    changed delay between downloads

--- apkmirror_crawler/apkmirror_crawler/settings.py
+++ apkmirror_crawler/apkmirror_crawler/settings.py
@@ -32,7 +32,7 @@ DOWNLOAD_WARNSIZE = 0
 CONCURRENT_REQUESTS = 1
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-DOWNLOAD_DELAY = 10
+DOWNLOAD_DELAY = 30
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 1
 
